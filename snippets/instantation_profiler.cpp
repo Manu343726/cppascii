ffc9b48dcc0716874a0c5e9c637d607461312a17
Manu343726 <Manu343726@gmail.com>
2014-01-18 22:03:30 +0100

    License header

--- snippets/instantation_profiler.cpp
+++ snippets/instantation_profiler.cpp
@@ -1,3 +1,16 @@
+/****************************************************************************
+* Snippets, ejemplos, y utilidades del curso de C++ orientado a videojuegos *
+* https://github.com/Manu343726/CppVideojuegos/                             *
+*                                                                           * 
+* Copyright © 2014 Manuel Sánchez Pérez                                     *
+*                                                                           *
+* This program is free software. It comes without any warranty, to          *
+* the extent permitted by applicable law. You can redistribute it           *
+* and/or modify it under the terms of the Do What The Fuck You Want         *
+* To Public License, Version 2, as published by Sam Hocevar. See            *
+* http://www.wtfpl.net/  and the COPYING file for more details.             *
+****************************************************************************/
+
 
 #include <iostream>
 #include <utility>
